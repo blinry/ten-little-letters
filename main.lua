afd457b3e7414dff24b153c17f396cb0976d3301
Sebastian Morr <sebastian@morr.cc>
2017-07-29 17:50:21 -0400

    Titel, balancing, hardcore mode

--- main.lua
+++ main.lua
@@ -81,10 +81,13 @@ function love.load()
 
     letters = ""
     energy = 100
+    mode = "title"
 
     --characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-    characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    allCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    characters = allCharacters
     newChar()
+    hint = "Click around,\nthen type your guess.\n\nCollect ten letters to win.\n\nWatch your energy bar."
 
     psystem = love.graphics.newParticleSystem(images.hit, 128)
     psystem:setParticleLifetime(1, 2) -- Particles live at least 2s and at most 5s.
@@ -111,9 +114,21 @@ function newChar()
 
     canvas:renderTo(function()
         love.graphics.clear(0, 0, 0, 0)
-        love.graphics.setColor(0, 0, 0)
+        love.graphics.setColor(255, 255, 255)
+        love.graphics.setFont(fonts.montserrat[1000])
         love.graphics.printf(char, 0, -100, 1000, "center")
     end)
+end
+
+function gameOver()
+    mode = "gameover"
+    show = false
+    hints = {
+        "The English word 'alphabet' is derived from the first two greek letters, alpha and beta.",
+        "The sentence 'The quick brown fox jumps over the lazy dog' is a pangram – it contains all letters of the alphabet.",
+        "A shorter pangram (containing all letters of the alphabet): 'Sphinx of black quartz, judge my vow'."
+    }
+    hint = "Game Over!\nClick to start a new game.\n\n"..hints[math.random(1, #hints)]
 end
 
 function love.update(dt)
@@ -122,20 +137,44 @@ end
 
 function love.keypressed(key)
     if key == "escape" then
+        if mode == "game" or mode == "win" then
+            mode = "title"
+        elseif mode == "title" then
+            love.event.quit()
+        end
+    end
+end
+
+function love.textinput(c)
+    if key == "escape" then
         love.window.setFullscreen(false)
         love.timer.sleep(0.1)
         love.event.quit()
     else
-        show = true
-        if key:upper() == char then
-            energy = math.min(100, energy + 50)
-            letters = letters..char
-            characters = characters:gsub(char, "")
-        else
-            if energy >= 10 then
-                energy = energy - 10
+        if not show then
+            show = true
+            if c == char or c:upper() == char then
+                energy = math.min(100, energy + 50)
+                letters = letters..char
+                characters = characters:gsub(char, "")
+                if #letters == 10 then
+                    mode = "win"
+                    show = false
+                    if hardcore then
+                        hint = "Wow, congratulations!\n\nDefinitely let me know that you made it this far!"
+                    else
+                        hint = "You did it!\nThanks for playing! <3\n\nI just enabled the 'hardcore mode', where you will also get numbers as well as some special characters! Good luck! :)"
+                    end
+                    hardcore = true
+                    allCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789+-#!?%$&"
+                end
             else
-                -- game over
+                if energy >= 10 then
+                    energy = energy - 10
+                else
+                    energy = 0
+                    gameOver()
+                end
             end
         end
     end
@@ -151,31 +190,49 @@ function love.mousepressed(x, y, button, touch)
     x, y = transformMouse(x, y)
     x = x-200
     y = y-200
-    if button == 1 then
-        if show then
+
+    if mode == "title" then
+        if button == 1 then
+            mode = "game"
+        end
+    elseif mode == "gameover" or mode == "win" then
+        if button == 1 then
+            hint = nil
+            mode = "game"
+            characters = allCharacters
             newChar()
-        else
-            if energy > 0 then
-                data = canvas:newImageData()
-                if x >= 0 and x <= 999 and y >= 0 and y <= 999 then
-                    r,g,b,a = data:getPixel(x, y)
-                    energy = energy - 0.5
-                    if a == 255 then
-                        love.audio.play(sounds.hit)
-                        psystem:setPosition(x+200, y+200)
-                        psystem:emit(1)
-                    else
-                        love.audio.play(sounds.miss)
-                    end
-                    table.insert(clicks, {x, y, a == 255})
-                end
+            energy = 100
+            letters = ""
+        end
+    else
+        if button == 1 then
+            if show then
+                newChar()
             else
-                -- game over
+                if energy > 0 then
+                    data = canvas:newImageData()
+                    if x >= 0 and x <= 999 and y >= 0 and y <= 999 then
+                        if hint then
+                            hint = nil
+                        end
+
+                        r,g,b,a = data:getPixel(x, y)
+                        energy = energy - 1
+                        if a == 255 then
+                            --sounds.hit:setPitch(1+0.2*(math.random()-0.5))
+                            love.audio.play(sounds.hit)
+                            psystem:setPosition(x+200, y+200)
+                            psystem:emit(1)
+                        else
+                            love.audio.play(sounds.miss)
+                        end
+                        table.insert(clicks, {x, y, a == 255})
+                    end
+                else
+                    gameOver()
+                end
             end
         end
-    end
-    if button == 2 then
-
     end
 end
 
@@ -186,36 +243,47 @@ function love.draw()
     love.graphics.translate(tx, ty)
     love.graphics.scale(bw/1400, bh/1400)
 
-
-    if show then
-        love.graphics.draw(canvas, 200, 200)
-        for key, click in pairs(clicks) do
-            hit = click[3]
-            if hit then
-                love.graphics.setColor(255, 255, 255)
-            else
-                love.graphics.setColor(0, 0, 0)
-            end
-            love.graphics.circle("fill", click[1]+200, click[2]+200, 10)
-            love.graphics.setColor(255, 255, 255)
-        end
+    if mode == "title" then
+        love.graphics.draw(images.title, 200, 200, 0, 0.5, 0.5)
     else
         love.graphics.setColor(0, 0, 0)
         love.graphics.rectangle("fill",200,200,1000,1000)
+
+        if show then
+            love.graphics.setColor(255, 255, 255, 255)
+            love.graphics.draw(canvas, 200, 200)
+            for key, click in pairs(clicks) do
+                hit = click[3]
+                if hit then
+                    love.graphics.setColor(0, 0, 0)
+                else
+                    love.graphics.setColor(255, 255, 255)
+                end
+                love.graphics.circle("fill", click[1]+200, click[2]+200, 10)
+                love.graphics.setColor(255, 255, 255)
+            end
+        end
+
+        love.graphics.setColor(0, 0, 0)
+        love.graphics.rectangle("fill",200,200+1000+50,energy*10,50)
+
+        love.graphics.setColor(0, 0, 0)
+        love.graphics.setFont(fonts.montserrat[50])
+        --love.graphics.print("Collect "..(10-#letters).." more characters to win: "..letters, 200, 100)
+        if hardcore then
+            love.graphics.print("Ten Little Characters: "..letters, 200, 100)
+        else
+            love.graphics.print("Ten Little Letters: "..letters, 200, 100)
+        end
+
+        love.graphics.setColor(255, 255, 255)
+        love.graphics.draw(psystem)
+
+        if hint then
+            love.graphics.setFont(fonts.montserrat[50])
+            love.graphics.printf(hint, 200+100, 200+320, 800, "center")
+        end
     end
-
-    love.graphics.setColor(0, 0, 0)
-    love.graphics.rectangle("fill",200,200+1000+50,energy*10,50)
-
-    love.graphics.setColor(0, 0, 0)
-    love.graphics.setFont(fonts.montserrat[50])
-    --love.graphics.print("Collect "..(10-#letters).." more characters to win: "..letters, 200, 100)
-    love.graphics.print("Ten Little Letters: "..letters, 200, 100)
-    love.graphics.setFont(fonts.montserrat[1000])
-
-    love.graphics.setColor(255, 255, 255)
-    love.graphics.draw(psystem)
-
 
     love.graphics.pop()
 end
